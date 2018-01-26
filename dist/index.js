bd4a6538456970c99fe25f477020fd1db3690890
刘韶 <liushao@liushaodeMacBook-Air.local>
2018-01-26 21:14:26 +0800

    初始化完成

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_native_1 = require("react-native");
+const App_1 = require("./src/App");
+react_native_1.AppRegistry.registerComponent('comic_reader', () => App_1.default);
+//# sourceMappingURL=index.js.map
